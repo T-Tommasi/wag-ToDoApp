4583b3e9c249778382b99591bfa0f23633ae42cd
T-Tommasi <ttommasi1945@gmail.com>
2025-01-31 16:20:05 +0100

    (MOD) Hard branch reset
    - The graphical interface was too tangled for it's objective
    - The actual logic was just as tangled, refactored from the beginning
    - Refactoring also the app grapics since some sides left much to desire

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
 import html from './template.html';
 import './style.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import { calendarInstance } from './calendarImporter';
-import { ListenerType,Appender,getInput } from './UiManager';
-
-function appInstance()  {
-    const NEWNOTESDISPLAY = document.querySelector('#recentNotes')
-    
-    function buttonListeners() {
-        const NOTEBUTTON = document.querySelector('#newNote');
-        const SUBMITBUTTON = document.querySelector('#submitNote');
-        getInput(SUBMITBUTTON);
-        ListenerType.noteMenu(NOTEBUTTON);
-        ListenerType.dialogCloser(SUBMITBUTTON,NEWNOTESDISPLAY);
-    };
-
-    function openNoteListener() {
-        const NEWNOTESLISTENERS = document.querySelectorAll('.newNoteGrabber');
-        const NOTEDISPLAY = document.querySelector('.taskContainer');
-        ListenerType.noteOpener(NEWNOTESLISTENERS,NOTEDISPLAY)
-    }
-
-    function instanceLauncher() {
-        Appender.cleanse(NEWNOTESDISPLAY);
-        buttonListeners();
-        calendarInstance;
-        Appender.initialize(NEWNOTESDISPLAY)
-        openNoteListener();
-    };
-
-    instanceLauncher()
-}
-
-appInstance()
